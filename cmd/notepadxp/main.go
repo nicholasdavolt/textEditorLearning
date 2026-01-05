73b898f57082818f77e302d8d3d432ab6cb72154
Nick Davolt <nicholasdavolt@gmail.com>
2026-01-05 15:33:22 -0600

    Feat:  app skeleton (fyne)

--- cmd/notepadxp/main.go
+++ cmd/notepadxp/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"image"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
@@ -10,10 +13,8 @@ import (
 func main() {
 	//sets basic app and creates window with title
 	a := app.New()
-	w := a.NewWindow("TextEditor - (untitled)")
+	w := a.NewWindow("TextEditor — (untitled)")
 	w.Resize(fyne.NewSize(800, 600))
-	w.SetFixedSize(false)
-
 	//creates the editor, sets wordwrap
 	editor := widget.NewMultiLineEntry()
 	editor.Wrapping = fyne.TextWrapWord
@@ -23,19 +24,23 @@ func main() {
 
 	fileMenu := fyne.NewMenu("File",
 		fyne.NewMenuItem("New", func() {}),
-		fyne.NewMenuItem("Open", func() {}),
+		fyne.NewMenuItem("Open…", func() {}),
 		fyne.NewMenuItem("Save", func() {}),
-		fyne.NewMenuItem("Save As", func() {}),
-		fyne.NewMenuItem("Close Window", w.Close))
+		fyne.NewMenuItem("Save As…", func() {}),
+		fyne.NewMenuItemSeparator(),
+		fyne.NewMenuItem("Close", w.Close))
 
 	editMenu := fyne.NewMenu("Edit",
 		fyne.NewMenuItem("Undo", func() {}),
 		fyne.NewMenuItem("Redo", func() {}),
+		fyne.NewMenuItemSeparator(),
 		fyne.NewMenuItem("Cut", func() {}),
 		fyne.NewMenuItem("Copy", func() {}),
 		fyne.NewMenuItem("Paste", func() {}),
 		fyne.NewMenuItem("Delete", func() {}),
+		fyne.NewMenuItemSeparator(),
 		fyne.NewMenuItem("Select All", func() {}),
+		fyne.NewMenuItemSeparator(),
 		fyne.NewMenuItem("Find…", func() {}),
 		fyne.NewMenuItem("Replace…", func() {}),
 		fyne.NewMenuItem("Go to Line…", func() {}))
@@ -51,8 +56,13 @@ func main() {
 	menuItems := []*fyne.Menu{fileMenu, editMenu, formatMenu, viewMenu, helpMenu}
 	mainMenu := fyne.NewMainMenu(menuItems...)
 	w.SetMainMenu(mainMenu)
+	minSizePlaceholder := canvas.NewImageFromImage(image.NewNRGBA(image.Rect(0, 0, 0, 0)))
+	minSizePlaceholder.SetMinSize(fyne.NewSize(400, 300))
+
 	content := container.NewBorder(nil, status, nil, nil, editor)
-	w.SetContent(content)
+	stackContainer := container.NewStack(minSizePlaceholder, content)
+
+	w.SetContent(stackContainer)
 	w.Canvas().Focus(editor)
 	w.ShowAndRun()
 
